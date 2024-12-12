2bde4b2da635607dc7abc29c5b956f23bf9a0756
Romanov Vladimir <romanov.v@eremex.ru>
2024-12-12 11:36:48 +0300

    first commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+
+namespace AvaloniaApplication1.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public MainWindowViewModel()
+    {
+        Tasks = CreateTasks();
+    }
+
+    private List<ProjectTask> CreateTasks()
+    {
+        List<ProjectTask> projectTasks = new();
+        projectTasks.Add(new() { Name = "'Trade Central' Web Site" });
+        projectTasks.Add(new() { Name = "'Six Sigma' Mobile App" });
+        projectTasks[0].Tasks.Add(new() { Name = "Market research", HighPriority = true });
+        projectTasks[0].Tasks.Add(new() { Name = "Create a sitemap" });
+        projectTasks[1].Tasks.Add(new() { Name = "Define objectives and requirements" });
+        projectTasks[1].Tasks.Add(new() { Name = "Design UI/UX" });
+        return projectTasks;    
+    }
+
+    public List<ProjectTask> Tasks { get; }
+
+    [RelayCommand]
+    public void Update()
+    {
+        Tasks[0].Tasks[1].HighPriority = !Tasks[0].Tasks[1].HighPriority;
+    }
+}
+
+public partial class ProjectTask : ObservableObject
+{
+    [ObservableProperty]  
+    private bool highPriority;
+
+    [ObservableProperty]
+    private string? name;
+
+
+    public List<ProjectTask> Tasks { get; } = new();
+
+    public bool HasTasks => Tasks.Count > 0;
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+
+using Eremex.AvaloniaUI.Controls.Common;
+using System.Globalization;
+using System;
+using Avalonia.Data.Converters;
+
+namespace AvaloniaApplication1.Views;
+
+public partial class MainWindow : MxWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
+
